0dd89d262095e69742f6bb7c5bd36fb22e085347
Ryan Fitzgerald <ryan@sessionwise.com>
2023-11-01 11:12:00 -0700

    image block fix

--- blocks/image/image.php
+++ blocks/image/image.php
@@ -15,11 +15,11 @@ if( get_field( 'custom_max_width' ) ){
     switch( $units ):
 
         case 'px':
-            $style .= 'max-width:' . get_field( 'max_width_px' ) . 'px; width: 100%;';
+            $style .= 'max-width:' . get_field( 'max_width_px' ) . 'px;width: 100%;';
             break;
     
         case '%':
-            $style .= 'max-width:' . get_field( 'max_width_percentage' ) . '%; width: 100%;';
+            $style .= 'max-width:' . get_field( 'max_width_percentage' ) . '%;width: 100%;';
             break;
     
     endswitch;
@@ -29,15 +29,15 @@ if( get_field( 'custom_max_width' ) ){
 switch( get_field( 'image_alignment' ) ){
 
     case 'left':
-        $style .= 'margin-right: auto; ';
+        $style .= 'margin-right: auto;';
         break;
 
     case 'right':
-        $style .= 'margin-left: auto; ';
+        $style .= 'margin-left: auto;';
         break;
 
     default:
-        $style .= 'margin-left: auto; margin-right: auto ';
+        $style .= 'margin-left: auto; margin-right: auto';
         break;
     
 }
@@ -50,9 +50,9 @@ $link = get_field( 'link' );
 
 $mobile_image = get_field( 'mobile_image' );
 
-echo '<div ' . ( !empty( $id ) ? 'id="' . $id . '" ' : '') . 'class="block-image' . $classes . '"' . ( !empty( $style ) ? ' style="' . $style . '"' : '' ) . '>';
+echo '<div ' . ( !empty( $id ) ? 'id="' . $id . '" ' : '') . 'class="block-image' . $classes . '">';
 
-    echo !empty( $fixed_aspect_ratio ) ? '<div class="force-aspect aspect-' . $fixed_aspect_ratio . '">' : '';
+    echo !empty( $fixed_aspect_ratio ) ? '<div style="' . $style . '"><div class="force-aspect aspect-' . $fixed_aspect_ratio . '">' : '';
 
     echo '<picture>';
 
@@ -63,10 +63,12 @@ echo '<div ' . ( !empty( $id ) ? 'id="' . $id . '" ' : '') . 'class="block-image
 
         echo get_field( 'mobile_image' ) ? '<source media="(max-width: 767px)" srcset="' . get_field( 'mobile_image' ) . '">' : '';
 
-        echo wp_get_attachment_image( $image, 'full' );
+        $img_props = !empty( $fixed_aspect_ratio ) ? [] : ['style' => $style];
+
+        echo wp_get_attachment_image( $image, 'full', false, $img_props );
 
     echo '</picture>';
 
-    echo !empty( $fixed_aspect_ratio ) ? '</div>' : '';
+    echo !empty( $fixed_aspect_ratio ) ? '</div></div>' : '';
 
 echo '</div>';
