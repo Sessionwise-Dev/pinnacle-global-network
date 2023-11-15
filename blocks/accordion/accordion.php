72984805ab34c86bec0dc22fc28a1bcdccf46cdf
Ryan Fitzgerald <ryan@sessionwise.com>
2023-11-15 13:52:53 -0700

    accordion fix

--- blocks/accordion/accordion.php
+++ blocks/accordion/accordion.php
@@ -9,7 +9,7 @@ echo '<div class="block-accordion">';
     foreach($accordion_items as $accordion) {
         echo '<div class="accordion-item">'.
             '<h3 class="accordion-title">'.$accordion->post_title.'<i class="fa-solid fa-angle-right"></i></h3>'.
-            '<div class="accordion-content">'.get_field('accordion_content', $accordion->ID).'</div>'.
+            '<div class="accordion-content">' . apply_filters( 'the_content', get_post_field( 'post_content', $location ) ) . '</div>'.
         '</div>';
     }
 echo '</div>';
